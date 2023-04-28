8859d846a5ae4c6e4110c80c4b786ec5a355a852
Leon Windhorst <leon@windhorst.dev>
2023-04-28 15:07:54 +0200

    style: run stylua on UpdateCheck.lua

--- packages/path-of-building/UpdateCheck.lua
+++ packages/path-of-building/UpdateCheck.lua
@@ -2,7 +2,7 @@
 --
 -- Module: Update Check
 -- Checks for updates
---
+
 local connectionProtocol, proxyURL = ...
 
 local updateAvailableMsg = "Update available!\n"
@@ -13,36 +13,36 @@ local curl = require("lcurl.safe")
 
 local globalRetryLimit = 10
 local function downloadFileText(source, file)
-    for i = 1, 5 do
-        if i > 1 then
-            ConPrintf("Retrying... (%d of 5)", i)
-        end
-        local text = ""
-        local easy = curl.easy()
-        local escapedUrl = source .. easy:escape(file)
-        easy:setopt_url(escapedUrl)
-        easy:setopt(curl.OPT_ACCEPT_ENCODING, "")
-        if connectionProtocol then
-            easy:setopt(curl.OPT_IPRESOLVE, connectionProtocol)
-        end
-        if proxyURL then
-            easy:setopt(curl.OPT_PROXY, proxyURL)
-        end
-        easy:setopt_writefunction(function(data)
-            text = text .. data
-            return true
-        end)
-        local _, error = easy:perform()
-        easy:close()
-        if not error then
-            return text
-        end
-        ConPrintf("Download failed (%s)", error:msg())
-        if globalRetryLimit == 0 or i == 5 then
-            return nil, error:msg()
-        end
-        globalRetryLimit = globalRetryLimit - 1
-    end
+	for i = 1, 5 do
+		if i > 1 then
+			ConPrintf("Retrying... (%d of 5)", i)
+		end
+		local text = ""
+		local easy = curl.easy()
+		local escapedUrl = source .. easy:escape(file)
+		easy:setopt_url(escapedUrl)
+		easy:setopt(curl.OPT_ACCEPT_ENCODING, "")
+		if connectionProtocol then
+			easy:setopt(curl.OPT_IPRESOLVE, connectionProtocol)
+		end
+		if proxyURL then
+			easy:setopt(curl.OPT_PROXY, proxyURL)
+		end
+		easy:setopt_writefunction(function(data)
+			text = text .. data
+			return true
+		end)
+		local _, error = easy:perform()
+		easy:close()
+		if not error then
+			return text
+		end
+		ConPrintf("Download failed (%s)", error:msg())
+		if globalRetryLimit == 0 or i == 5 then
+			return nil, error:msg()
+		end
+		globalRetryLimit = globalRetryLimit - 1
+	end
 end
 
 ConPrintf("Checking for update...")
@@ -55,22 +55,22 @@ local localBranch
 local localManXML = xml.LoadXMLFile(scriptPath .. "/manifest.xml")
 local localSource
 if localManXML and localManXML[1].elem == "PoBVersion" then
-    for _, node in ipairs(localManXML[1]) do
-        if type(node) == "table" then
-            if node.elem == "Version" then
-                localVer = node.attrib.number
-                localBranch = node.attrib.branch
-            elseif node.elem == "Source" then
-                if node.attrib.part == "default" then
-                    localSource = node.attrib.url
-                end
-            end
-        end
-    end
+	for _, node in ipairs(localManXML[1]) do
+		if type(node) == "table" then
+			if node.elem == "Version" then
+				localVer = node.attrib.number
+				localBranch = node.attrib.branch
+			elseif node.elem == "Source" then
+				if node.attrib.part == "default" then
+					localSource = node.attrib.url
+				end
+			end
+		end
+	end
 end
 if not localVer then
-    ConPrintf("Update check failed: invalid local manifest")
-    return nil, updateCheckFailedMsg .. "Invalid local manifest"
+	ConPrintf("Update check failed: invalid local manifest")
+	return nil, updateCheckFailedMsg .. "Invalid local manifest"
 end
 localSource = localSource:gsub("{branch}", localBranch)
 
@@ -78,30 +78,30 @@ localSource = localSource:gsub("{branch}", localBranch)
 local remoteVer
 local remoteManText, errMsg = downloadFileText(localSource, "manifest.xml")
 if not remoteManText then
-    ConPrintf("Update check failed: could not download version manifest")
-    return nil,
-        updateCheckFailedMsg
-            .. "Could not download version manifest.\nReason: "
-            .. errMsg
-            .. "\nCheck your internet connectivity.\nIf you are using a proxy, specify it in Options."
+	ConPrintf("Update check failed: could not download version manifest")
+	return nil,
+		updateCheckFailedMsg
+			.. "Could not download version manifest.\nReason: "
+			.. errMsg
+			.. "\nCheck your internet connectivity.\nIf you are using a proxy, specify it in Options."
 end
 local remoteManXML = xml.ParseXML(remoteManText)
 if remoteManXML and remoteManXML[1].elem == "PoBVersion" then
-    for _, node in ipairs(remoteManXML[1]) do
-        if type(node) == "table" then
-            if node.elem == "Version" then
-                remoteVer = node.attrib.number
-            end
-        end
-    end
+	for _, node in ipairs(remoteManXML[1]) do
+		if type(node) == "table" then
+			if node.elem == "Version" then
+				remoteVer = node.attrib.number
+			end
+		end
+	end
 end
 if not remoteVer then
-    ConPrintf("Update check failed: invalid remote manifest")
-    return nil, updateCheckFailedMsg .. "Invalid remote manifest"
+	ConPrintf("Update check failed: invalid remote manifest")
+	return nil, updateCheckFailedMsg .. "Invalid remote manifest"
 end
 
 -- Display notification if there is an update available.
 -- Hijacking the error msg is a little scuffed, but it is what it is.
 if remoteVer > localVer then
-    return nil, updateAvailableMsg .. "Latest version: " .. remoteVer .. "\nUpdate your nix package you lazy fuck!"
+	return nil, updateAvailableMsg .. "Latest version: " .. remoteVer .. "\nUpdate your nix package you lazy fuck!"
 end
