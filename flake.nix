1ae04a5cd3b4ed82462acae0fe5ae9bd217b47d8
Leon Windhorst <leon@windhorst.dev>
2023-04-28 16:58:32 +0200

    feat: add nixpkgs input for use with nix shell command

--- flake.nix
+++ flake.nix
@@ -1,7 +1,22 @@
 {
   description = "Simple flake with a few custom packages I use";
 
-  outputs = { self }: {
-    extraPkgs = ./packages;
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
   };
+
+  outputs = { self, nixpkgs }: 
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; }; 
+
+    in {
+      # This can be run via the 'nix shell' command
+      packages.${system} = import ./packages { inherit pkgs; };
+
+      # Use this for importing packages to another flake
+      extraPkgs = ./packages;
+    };
 }
