ca3bc7be904e7658aa7973df324ece0e24bd92da
Leon Windhorst <leon@windhorst.dev>
2023-05-30 18:03:34 +0200

    feat: add tela circle icon theme

--- packages/default.nix
+++ packages/default.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
 
 {
-  nitch                 = pkgs.callPackage ./nitch {};
-  odin                  = pkgs.callPackage ./odin {};
-  ols                   = pkgs.callPackage ./ols {};
-  papirus-icon-theme    = pkgs.callPackage ./papirus-icon-theme { inherit pkgs; };
-  path-of-building      = pkgs.callPackage ./path-of-building {};
-  sddm-rose-pine        = pkgs.callPackage ./sddm-rose-pine {};
+  nitch                   = pkgs.callPackage ./nitch {};
+  odin                    = pkgs.callPackage ./odin {};
+  ols                     = pkgs.callPackage ./ols {};
+  papirus-icon-theme      = pkgs.callPackage ./papirus-icon-theme { inherit pkgs; };
+  path-of-building        = pkgs.callPackage ./path-of-building {};
+  sddm-rose-pine          = pkgs.callPackage ./sddm-rose-pine {};
+  tela-circle-icon-theme  = pkgs.callPackage ./tela-circle-icon-theme { inherit pkgs; };
 }
