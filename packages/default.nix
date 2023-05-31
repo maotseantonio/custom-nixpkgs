2948f235cd89479d265655c40ec911916da0c6a0
Leon Windhorst <leon@windhorst.dev>
2023-05-31 09:20:04 +0200

    chore: remove tela circle

--- packages/default.nix
+++ packages/default.nix
@@ -7,5 +7,4 @@
   papirus-icon-theme      = pkgs.callPackage ./papirus-icon-theme { inherit pkgs; };
   path-of-building        = pkgs.callPackage ./path-of-building {};
   sddm-rose-pine          = pkgs.callPackage ./sddm-rose-pine {};
-  tela-circle-icon-theme  = pkgs.callPackage ./tela-circle-icon-theme { inherit pkgs; };
 }
